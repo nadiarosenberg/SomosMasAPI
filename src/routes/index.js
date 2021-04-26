bc35dcb43b8034639ae4f51c6a5a7e97614fc4c5
Elias Moura <d.eliasmoura2@gmail.com>
2021-04-26 20:58:22 -0300

    update endpoint GET /

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.status(202).json({ title: 'Express' });
 });
 
 module.exports = router;
