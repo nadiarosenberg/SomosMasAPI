5ded14d5d02cd5e0e487addcc3d1f1e18c5ae544
Dominguez Gaston Matias <gastonmd14@gmail.com>
2021-04-27 15:14:32 -0300

    User Controller Created

---
+++ controllers/userController.js
@@ -0,0 +1,22 @@
+
+const db = require('../src/models')
+
+module.exports = {
+
+    create: (req, res, next) => {
+
+        db.User.create({
+
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            password: req.body.password,
+            photo: req.body.photo,
+
+        })
+
+        .then((r) => {res.status(200).json(r)})
+        .catch((e) => {res.status(500).json(e)})
+
+    }
+}
