54f288e1ce60f72fe1d16929c81a6ad129332706
Elias Moura <d.eliasmoura2@gmail.com>
2021-04-28 23:01:55 -0300

    comment and require removed

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,6 @@
-const { Organization } = require('../models/index');
 var express = require('express');
 var router = express.Router();
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
   res.status(200).json({ title: 'Express' });
 });
