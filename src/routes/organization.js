2b68dd859000343e30b373ae19e0dff27af37c5f
Elias Moura <d.eliasmoura2@gmail.com>
2021-04-28 23:00:14 -0300

    comment removed

--- src/routes/organization.js
+++ src/routes/organization.js
@@ -1,7 +1,7 @@
 const organizationCtrl = require('../controllers/organization');
 var express = require('express');
 var router = express.Router();
-/* GET home page. */
+
 router.get('/', organizationCtrl.findAll);
 
 module.exports = router;
