02056bb8cac307b91bbed0f73f23ccb1f9fc529a
Elias Moura <d.eliasmoura2@gmail.com>
2021-04-28 22:21:57 -0300

    the password attribute was restored

--- config/config.js
+++ config/config.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 module.exports = {
   "development": {
     "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
     "host": process.env.DB_HOST,
     "port": process.env.DB_PORT,
     "dialect": "mysql",
