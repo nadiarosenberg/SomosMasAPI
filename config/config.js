f57688ada48d691255633aa5f05ec5a8f8542c53
Elias Moura <d.eliasmoura2@gmail.com>
2021-04-28 22:17:30 -0300

    indentation of the config.js file fixed

--- config/config.js
+++ config/config.js
@@ -1,40 +1,35 @@
 require('dotenv').config()
 
 module.exports = {
-    "development": {
-        "username": process.env.DB_USER,
-        "password": process.env.DB_PASSWORD,
-        "database":  process.env.DB_NAME,
-        "host": process.env.DB_HOST,
-        "port": process.env.DB_PORT,
-        "dialect": "mysql",
-        pool:{
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000
-        },
-        logging: false,
-        // config seeds
-            seederStorage: 'sequelize',
-            seederStorageTableName: 'seeds',
-        
-        // config migrations
-            migrationStorage: 'sequelize',
-            migrationStorageTableName: 'migrations'
+  "development": {
+    "username": process.env.DB_USER,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "dialect": "mysql",
+    pool:{
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
     },
-    "test": {
-        "username": process.env.DB_USER,
-        "password": process.env.DB_PASSWORD,
-        "database": process.env.DB_NAME,
-        "host": process.env.DB_HOST,
-        "dialect": "mysql"
-    },
-    "production": {
-        "username": process.env.DB_USER,
-        "password": process.env.DB_PASSWORD,
-        "database": process.env.DB_NAME,
-        "host": process.env.DB_HOST,
-        "dialect": "mysql"
-    }
+    logging: false,
+    seederStorage: "sequelize",
+    seederStorageTableName: "seeds",
+    migrationStorage: "sequelize",
+    migrationStorageTableName: "migrations"
+  },
+  "test": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": "mysql"
+  }
 }
