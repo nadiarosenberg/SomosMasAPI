648981b7bd86fe30b2c3400d4eaabff1587b9b06
gaston <sleepwalkgaston08@gmail.com>
2021-04-29 22:55:30 -0300

    change jwt validation and add user validation with sequelize

--- src/routes/authUser.js
+++ src/routes/authUser.js
@@ -6,11 +6,44 @@ const User = db.User;
 
 const authUser = async (req, res, next) => {
     const token = req.get('token')
-    jwt.verify(token, 'mi-secreto', (error, decoded) => {
-        if (error) {
-            return res.status(401).json({ error })
+    let userId;
+    let error;
+    jwt.verify(token, 'mi-secreto', (err, decoded) => {
+        if (err) {
+            error = err;
+        }else{
+            userId = decoded.id;
         }
     })
+    if(error){
+        res.status(403).json({ error })
+    }else{
+        const arrPath = req.originalUrl.split('/');
+        const hasPublicPath = arrPath.includes('public')
+        if(hasPublicPath){
+            next()
+        }else{
+            const userJoinRole = await User.findOne({ 
+                where:{
+                    id: userId
+                },
+                include: {
+                model: Role,
+                as: 'role'
+                }
+            });
+            const role = userJoinRole.dataValues.role.name
+            if(role==='Admin'){
+                console.log("PASA POR ACA")
+                next()
+            }else{
+                return res.status(403).json({
+                    ok: false,
+                    error: `${role} does not have permission to access this content`
+                })
+            }
+        }
+    }
 }
 
 
