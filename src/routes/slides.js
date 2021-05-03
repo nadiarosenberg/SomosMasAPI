f774ec37c5c5c3e0cb453753c2e2f151577eba27
Nadia Rosenberg <nadia>
2021-05-03 16:29:59 -0300

    Create and modify controller

--- src/routes/slides.js
+++ src/routes/slides.js
@@ -1,67 +1,11 @@
-var express = require('express');
-var router = express.Router();
-const {Slide} = require('../models');
+const express = require('express');
+const router = express.Router();
+const slides = require('../controllers/slides');
 
-router.post('/', async(req, res, next) => {
-  const body_data = req.body;
-  try{
-      const slide = await Slide.create({
-        imageUrl: body_data.imageUrl,
-        text: body_data.text,
-        order: body_data.order,
-        organizationId: body_data.organizationId
-      })
-      res.json(slide);
-  }catch (err){
-      console.log(err);
-      res.send('Error posting slide')
-  }
-});
-
-router.get('/', async(req, res, next) => {
-  try{
-      const slide = await Slide.findAll();
-      res.json(slide);
-  }catch (err){
-      console.log(err);
-      res.send('Error getting slides')
-  }
-});
-
-router.get('/:id', async(req, res, next) => {
-  try{
-      const slide = await Slide.findOne(req.body, {
-        where: {id: req.params.id}
-      })
-      res.json(slide);
-  }catch (err){
-      console.log(err);
-      res.send('Error getting slide')
-  }
-});
-
-router.put('/:id', async(req, res, next) => {
-  try{
-      const slide = await Slide.update(req.body, {
-        where: {id: req.params.id}
-      })
-      res.json(slide);
-  }catch(err){
-      console.log(err);
-      res.send('Error updating slide')
-  }
-});
-
-router.delete('/:id', async(req, res, next) => {
-  try{
-      const slide = await Slide.destroy({
-        where: {id: req.params.id}
-      })
-      res.json(slide);
-  }catch(err){
-    console.log(err);
-    res.send('Error deleting slide')
-  }
-});
+router.post('/', slides.create);
+router.get('/', slides.findAll);
+router.get('/:id', slides.findOne);
+router.put('/:id', slides.update);
+router.delete('/:id', slides.destroy);
 
 module.exports = router;
--- src/seeders/20210429173215-slide.js
+++ src/seeders/20210429173215-slide.js
@@ -6,14 +6,14 @@ module.exports = {
       imageUrl: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
       text: 'text',
       order: 1,
-      organizationId: 1,
+      //organizationId: 1,
       createdAt: new Date(),
       updatedAt: new Date()
     },{
       imageUrl: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
       text: 'text',
       order: 2,
-      organizationId: 2,
+      //organizationId: 2,
       createdAt: new Date(),
       updatedAt: new Date()
     }
