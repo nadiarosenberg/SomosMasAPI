ef293c31874f10f40fa6dcba1708ba7173048e89
gaston <sleepwalkgaston08@gmail.com>
2021-05-02 21:38:52 -0300

    Change function to log endpoint with pino

--- src/utils/pinoLogger.js
+++ src/utils/pinoLogger.js
@@ -1,5 +1,39 @@
 const pino = require('pino')
+var app = require('../server');
 
-const pinoLogger = pino({})
+const pinoLogger = pino({
+  prettyPrint: { colorize: true }
+})
+
+// print endpoints with method
+function print (path, layer) {
+  if (layer.route) {
+    layer.route.stack.forEach(print.bind(null, path.concat(split(layer.route.path))))
+  } else if (layer.name === 'router' && layer.handle.stack) {
+    layer.handle.stack.forEach(print.bind(null, path.concat(split(layer.regexp))))
+  } else if (layer.method) {
+    pinoLogger.info('%s /%s',
+      layer.method.toUpperCase(),
+      path.concat(split(layer.regexp)).filter(Boolean).join('/'))
+  }
+}
+  
+function split (thing) {
+  if (typeof thing === 'string') {
+    return thing.split('/')
+  } else if (thing.fast_slash) {
+    return ''
+  } else {
+    var match = thing.toString()
+      .replace('\\/?', '')
+      .replace('(?=\\/|$)', '$')
+      .match(/^\/\^((?:\\[.*+?^${}()|[\]\\\/]|[^.*+?^${}()|[\]\\\/])*)\$\//)
+    return match
+      ? match[1].replace(/\\(.)/g, '$1').split('/')
+      : '<complex:' + thing.toString() + '>'
+  }
+}
+  
+app._router.stack.forEach(print.bind(null, []))
 
 module.exports = pinoLogger
