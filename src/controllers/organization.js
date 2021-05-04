e578921cea32fd9e3e22d372d4840fb57eebb307
Elias Moura <d.eliasmoura2@gmail.com>
2021-05-04 17:03:38 -0300

    add DELETE /organizations/:id in controllers/organization.js

--- src/controllers/organization.js
+++ src/controllers/organization.js
@@ -60,4 +60,26 @@ expressRouter.put('/public/:id', async (req, res, next) => {
   }
 });
 
+expressRouter.delete('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    let organization = await handler.getOrganizationById(id);
+
+    if (!organization) {
+      logger.warn('Organization not found');
+      res.status(404).json({ message: 'Organization not found' });
+      return;
+    }
+
+    await handler.deleteOrganization(id);
+
+    logger.info('Organization deleted successfully');
+    res.status(200).json({ message: 'Organization deleted successfully' });
+  } catch (error) {
+    logger.error(error.message);
+    res.status(500).json({ message: error.message});
+  }
+})
+
 module.exports = expressRouter;
