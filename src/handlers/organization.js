2dd4a789ac40ec7357d2441158778ebca4363e00
Elias Moura <d.eliasmoura2@gmail.com>
2021-05-04 17:37:28 -0300

    add restoreOrganization(id) method in handlers/organization.js

--- src/handlers/organization.js
+++ src/handlers/organization.js
@@ -8,27 +8,33 @@ const createOrganization = async (organization) => {
 const getAllOrganizations = async () => {
   const organizations = await repository.getAll();
   return organizations;
-}
+};
 
 const getOrganizationById = async (id) => {
   const organization = await repository.getOne(id);
   return organization;
-}
+};
 
 const updateOrganization = async (id,properties) => {
   const organization = await repository.update(id, properties);
   return organization;
-}
+};
 
 const deleteOrganization = async (id) => {
   const organization = await repository.destroy(id);
   return organization;
-}
+};
+
+const retoreOrganization = async (id) => {
+  const organization = await repository.restore(id);
+  return organization;
+};
 
 module.exports = {
   createOrganization,
   getAllOrganizations,
   getOrganizationById,
   updateOrganization,
-  deleteOrganization
+  deleteOrganization,
+  retoreOrganization
 };
