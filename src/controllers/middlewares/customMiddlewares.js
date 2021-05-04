d10f986f59c231c4c16e2e45d50a59822ee5c5c0
Elias Moura <d.eliasmoura2@gmail.com>
2021-05-04 11:51:26 -0300

    add middlewares folder with an example in controllers folder

---
+++ src/controllers/middlewares/customMiddlewares.js
@@ -0,0 +1,14 @@
+const organizationsMiddles = {
+  middlewareOne: () => {},
+  middlewareTwo: () => {}
+}
+
+const usersMiddles = {
+  middlewareOne: () => {},
+  middlewareTwo: () => {}
+};
+
+module.exports = {
+  organizationsMiddles,
+  usersMiddles
+}
