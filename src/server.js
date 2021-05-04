67788232427aa12d09fa7465b70c9c64758f51b2
Elias Moura <d.eliasmoura2@gmail.com>
2021-05-04 14:33:47 -0300

    delete comments

--- src/server.js
+++ src/server.js
@@ -34,7 +34,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-// app.use('/organizations', organizationRouter);
 app.use('/testimonials', testimonialsRouter);
 app.use('/auth', authRouter);
 app.use('/members', membersRouter);
