b26597509a655b1a2df775b9cb1979074b8654c0
Nadia Rosenberg <nadia>
2021-05-05 19:10:12 -0300

    Change body_data and id_body to camelCase

--- config/database/migrations/20210428001140-create-testimonials.js
+++ config/database/migrations/20210428001140-create-testimonials.js
@@ -1,12 +1,12 @@
-'use strict';
+"use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Testimonials', {
+    await queryInterface.createTable("Testimonials", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       name: {
         type: Sequelize.STRING,
@@ -14,34 +14,34 @@ module.exports = {
         validate: {
           notNull: {
             args: true,
-            msg: 'Name is required'
-          }
-        }
+            msg: "Name is required",
+          },
+        },
       },
       image: {
         type: Sequelize.STRING,
-        validate:{
-          is:{
+        validate: {
+          is: {
             args: /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png)/g,
-            msg: "You must enter an image url"
-          }
-        }
+            msg: "You must enter an image url",
+          },
+        },
       },
       content: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       createdAt: {
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
-      updatedAt:{
-        type: Sequelize.DATE
+      updatedAt: {
+        type: Sequelize.DATE,
       },
-      deletedAt:{
-        type: Sequelize.DATE
-      }
+      deletedAt: {
+        type: Sequelize.DATE,
+      },
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Testimonials');
-  }
+    await queryInterface.dropTable("Testimonials");
+  },
 };
