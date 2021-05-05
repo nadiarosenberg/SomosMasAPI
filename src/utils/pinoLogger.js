3635b0bb409e3caa4a1d4d7b459fe1747b9d3fa4
gaston <sleepwalkgaston08@gmail.com>
2021-05-05 18:57:26 -0300

    Change logic to log endpoints

--- src/utils/pinoLogger.js
+++ src/utils/pinoLogger.js
@@ -1,39 +1,23 @@
-const pino = require('pino')
-var app = require('../server');
+const pino = require("pino");
+const listEndpoints = require("express-list-endpoints");
 
 const pinoLogger = pino({
-  prettyPrint: { colorize: true }
-})
+  prettyPrint: {
+    colorize: true,
+    ignore: "pid,hostname,level,time",
+  },
+});
 
-// print endpoints with method
-function print (path, layer) {
-  if (layer.route) {
-    layer.route.stack.forEach(print.bind(null, path.concat(split(layer.route.path))))
-  } else if (layer.name === 'router' && layer.handle.stack) {
-    layer.handle.stack.forEach(print.bind(null, path.concat(split(layer.regexp))))
-  } else if (layer.method) {
-    pinoLogger.info('%s /%s',
-      layer.method.toUpperCase(),
-      path.concat(split(layer.regexp)).filter(Boolean).join('/'))
-  }
-}
-  
-function split (thing) {
-  if (typeof thing === 'string') {
-    return thing.split('/')
-  } else if (thing.fast_slash) {
-    return ''
-  } else {
-    var match = thing.toString()
-      .replace('\\/?', '')
-      .replace('(?=\\/|$)', '$')
-      .match(/^\/\^((?:\\[.*+?^${}()|[\]\\\/]|[^.*+?^${}()|[\]\\\/])*)\$\//)
-    return match
-      ? match[1].replace(/\\(.)/g, '$1').split('/')
-      : '<complex:' + thing.toString() + '>'
-  }
-}
-  
-app._router.stack.forEach(print.bind(null, []))
+const logWithPino = (app) => {
+  listEndpoints(app).forEach((endpoint) => {
+    if (endpoint.methods.length > 1) {
+      endpoint.methods.forEach((meth) => {
+        pinoLogger.info(`[${meth}] ${endpoint.path}`);
+      });
+    } else {
+      pinoLogger.info(`[${endpoint.methods[0]}] ${endpoint.path}`);
+    }
+  });
+};
 
-module.exports = pinoLogger
+module.exports = logWithPino;
