958743445e5e3a12dba6806b1ce5a5c9d7733c5e
Elias Moura <d.eliasmoura2@gmail.com>
2021-05-05 15:16:18 -0300

    comments deleted

--- src/controllers/activities.js
+++ src/controllers/activities.js
@@ -5,10 +5,6 @@ const logger = require('../utils/pinoLogger');
 const roleIdMiddleware = require("../controllers/middlewares/roleId.js");
 const { activityValidationRules, validate } = require('../controllers/middlewares/activityValidation');
 
-// borrar
-// const Activity = db.activity;
-// const Op = db.Sequelize.Op;
-
 expressRouter.post('/',  roleIdMiddleware, activityValidationRules(), validate, async (req, res) => {
   try {
     const activity = {
@@ -38,104 +34,5 @@ expressRouter.post('/',  roleIdMiddleware, activityValidationRules(), validate, 
     res.status(500).json({ message: error.message});
   }
 })
-
-// exports.create = (req, res) => {
-  // Create a activity
-
-  /*--------POST-------------------------------*/
-  // Save Activity in the database
-//   Activity.create(activity)
-//     .then(data => {
-//       res.send(data);
-//     })
-//     .catch(err => {
-//       res.status(500).send({
-//         message:
-//           err.message || "Some error occurred while creating the Activity."
-//       });
-//     });
-// };
-
-// exports.findAll = (req, res) => {
-
-//   Activity.findAll({ paranoid: false })
-//     .then(data => {
-//       res.send(data);
-//     })
-//     .catch(err => {
-//       res.status(500).send({
-//         message:
-//           err.message || "Some error occurred while retrieving Activity."
-//       });
-//     });
-// };
-
-// exports.findOne = (req, res) => {
-//   const id = req.params.id;
-
-//   Activity.findByPk(id,)
-//     .then(data => {
-//       if (data === null) {
-//         res.status(404).send({
-//           message: `Cannot find Activity with id = ${id}`
-//         })
-//         return;
-//       }
-//       res.send(data);
-//     })
-//     .catch(err => {
-//       console.log(err);
-//       res.status(500).send({
-//         message: "Error retrieving Activity with id = " + id
-//       });
-//     });
-// };
-
-// exports.update = (req, res) => {
-//   const id = req.params.id;
-//   Activity.update(req.body, {
-//     where: { id: id }
-//   })
-//     .then(num => {
-//       if (num == 1) {
-//         res.send({
-//           message: "Activity was updated successfully."
-//         });
-//       } else {
-//         res.send({
-//           message: `Cannot update Activity with id = ${id}. Activity was not found or req.body is empty!`
-//         });
-//       }
-//     })
-//     .catch(err => {
-//       res.status(500).send({
-//         message: "Error updating Activity with id = " + id
-//       });
-//     });
-// };
-
-// exports.delete = (req, res) => {
-//   const id = req.params.id;
-
-//   Activity.destroy({
-//     where: { id: id }
-//   })
-//     .then(num => {
-//       if (num == 1) {
-//         res.send({
-//           message: "Activity was deleted successfully!"
-//         });
-//       } else {
-//         res.send({
-//           message: `Cannot delete Activity with id = ${id}. Activity was not found!`
-//         });
-//       }
-//     })
-//     .catch(err => {
-//       res.status(500).send({
-//         message: "Could not delete Activity with id = " + id
-//       });
-//     });
-// };
 
 module.exports = expressRouter;
