03539f63e20522579e132147b0416b7cd87ac1af
Elias Moura <d.eliasmoura2@gmail.com>
2021-05-05 14:35:31 -0300

    add persist(activity) in services/repositories/activity.js

--- src/services/repositories/activity.js
+++ src/services/repositories/activity.js
@@ -1,9 +1,9 @@
-const { Activity } = require('../../models');
+const { Activities } = require('../../models');
 const logger = require('../../utils/pinoLogger');
 
 const persist = async (activityToPersist) => {
   try {
-    const result = await Activity.create(activityToPersist);
+    const result = await Activities.create(activityToPersist);
     return result;
   } catch (error) {
     logger.error(error.message);
