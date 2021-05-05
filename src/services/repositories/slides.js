c61e8d0dac976db0d745db9d88aeecce16f33ba7
Nadia Rosenberg <nadia>
2021-05-05 01:01:44 -0300

    Create repository for slides

--- src/services/repositories/slides.js
+++ src/services/repositories/slides.js
@@ -1,95 +1,65 @@
-const {Slides} = require("../models/index");
-const Slide = db.Slide;
+const {Slide} = require("../models/index");
 
 const persist = async (slide) => {
-  const body_data = req.body;
-  try {
-    const slide = await Slide.create({
-      imageUrl: body_data.imageUrl,
-      text: body_data.text,
-      order: body_data.order,
-      organizationId: body_data.organizationId,
-    });
-    res.json(slide);
-  } catch (err) {
-    console.log(err);
-    res.send("Error posting slide");
-  }
+    try {
+        const result = await Slide.create(slide);
+        return result;
+    }catch (error) {
+        console.log(error);
+        res.send("Error posting slides");
+    }
 };
-
-const findAll = async (req, res) => {
+    
+const findAll = async () => {
   try {
-    const slide = await Slide.findAll({
-      order: [["order", "ASC"]],
-    });
-    res.json(slide);
-  } catch (err) {
-    console.log(err);
+    const result = await Slide.findAll();
+    return result;
+  } catch (error) {
+    console.log(error);
     res.send("Error getting slides");
   }
 };
 
-const findOne = async (req, res) => {
-  const id_body = req.params.id;
+const findOne = async (slideId) => {
   try {
-    const slide = await Slide.findOne({
-      where: { id: id_body },
+    const result = await Slide.findOne({
+        where: {slideId}
     });
-    if (slide) {
-      res.json(slide);
-    } else {
-      res.status(404).json("Slide not found");
-    }
-  } catch (err) {
-    console.log(err);
+    return result
+  } catch (error) {
+    console.log(error);
     res.send("Error getting slide");
   }
 };
 
-const update = async (req, res) => {
-  const id_body = req.params.id;
+const update = async (slideId, slide) => {
   try {
-    const slide = await Slide.findOne({
-      where: { id: id_body },
-    });
-    if (slide) {
-      const slide = await Slide.update(req.body, {
-        where: { id: req.params.id },
-      });
-      res.status(200).json("Slide updated successfully");
-    } else {
-      res.status(404).json("Slide does not exist");
-    }
-  } catch (err) {
-    console.log(err);
+      const result = await Slide.update({
+          where: {slideId}
+      })
+      return result
+  } catch (error) {
+    console.log(error);
     res.send("Error updating slide");
   }
 };
 
-const destroy = async (req, res) => {
-  const id_body = req.params.id;
+const destroy = async (slideId) => {
   try {
-    const slide = await Slide.findOne({
-      where: { id: id_body },
-    });
-    if (slide) {
-      const slide = await Slide.destroy({
-        where: { id: id_body },
-      });
-      res.status(200).json("Slide deleted successfully");
-    } else {
-      res.status(404).json("Slide does not exist");
-    }
-  } catch (err) {
-    console.log(err);
+    const result = await Slide.destroy({
+      where: {slideId},
+    })
+    return result
+  } catch (error) {
+    console.log(error);
     res.send("Error deleting slide");
   }
 };
 
 module.exports = {
-  create,
-  findAll,
-  findOne,
-  update,
-  destroy,
+    persist,
+    findAll,
+    findOne,
+    update,
+    destroy
 };
