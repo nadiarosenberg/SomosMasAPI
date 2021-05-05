5951e6b29ba0e918c2d8cd45541b041085556d54
Elias Moura <d.eliasmoura2@gmail.com>
2021-05-05 12:10:29 -0300

    add createActivity(activity) method in handlers/activity.js

---
+++ src/handlers/activity.js
@@ -0,0 +1,12 @@
+const repository = require('./../services/repositories/activity');
+
+const createActivity = async (activity) => {
+  {
+    const createdActivity = await repository.persist(activity);
+    return createdActivity;
+  }
+};
+
+module.exports = {
+  createActivity
+}
