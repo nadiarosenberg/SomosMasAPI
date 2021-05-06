c6ce76ff4cc39fb56a6baa14367d3f3b2cef608a
Elias Moura <d.eliasmoura2@gmail.com>
2021-05-06 08:52:04 -0300

    fixed error categoryId

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const { userValidationRules, validate } = require('../utils/userValidation')
+const { User } = require('../models/index')
+const bcrypt = require('bcryptjs');
+const roleIdMiddleware = require("../controllers/middlewares/roleId.js");
+const users = require("../controllers/users.js");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post('/auth/register', userValidationRules(), validate, async (req, res, next) => {
+  const newUser = req.body
+  const data = await User.create({
+    firstName: newUser.firstName,
+    lastName: newUser.lastName,
+    email: newUser.email,
+    password: bcrypt.hashSync(newUser.password, 8),
+    createdAt: new Date(),
+    updatedAt: new Date()
+  })
+  res.json(data)
+})
+
+router.get("/:id", roleIdMiddleware, users.findOne);
+
+router.get("/", users.findAll);
 
 module.exports = router;
