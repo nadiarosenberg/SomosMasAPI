f35d3831751ad875f57f80137d6a612815240690
Christian Sosa <chrisosa@everis.com>
2021-05-06 10:50:19 -0300

    Fixed PR requirements

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,11 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const {
     userValidationRules,
     validate,
     userValidationPutRules
 } = require('./middlewares/userValidation')
 const handler = require('./../handlers/users');
+
+const wasUpdated = (result, req , res) => {
+    result[0] === 1 ? res.json({
+        ok: true,
+        msj: 'user updated successfully'
+    }) : res.status(400).json({
+        ok: false,
+        msj: 'failed to update user'
+    });
+}
 
 router.post('/auth/register', userValidationRules(), validate, async (req, res, next) => {
     try {
@@ -23,15 +33,9 @@ router.post('/auth/register', userValidationRules(), validate, async (req, res, 
 router.put('/:id', userValidationPutRules(), validate, async (req, res, next) => {
     try {
         const user = req.body;
-        const id = req.params.id
+        const id = req.params.id;
         const result = await handler.updateUser(id, user);
-        result[0] === 1 ? res.json({
-            ok: true,
-            msj: 'user updated successfully'
-          }) : res.status(400).json({
-            ok: false,
-            msj: 'failed to update user'
-          });
+        wasUpdated(result, req , res);
     } catch (e) {
         res.status(400).json({
             ok: false,
