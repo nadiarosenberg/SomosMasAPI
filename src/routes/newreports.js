39dd631a8baa456cd9bd2a6b205f3756bf251854
Elias Moura <d.eliasmoura2@gmail.com>
2021-05-06 12:38:17 -0300

    add GET /activities/:id in controllers/newreport.js- Get a newreport

--- src/routes/newreports.js
+++ src/routes/newreports.js
@@ -4,8 +4,6 @@ const router = express.Router();
 
 router.get("/", newreports.findAll);
 
-router.get("/:id", newreports.findOne);
-
 router.put("/:id", newreports.update);
 
 module.exports = router;
