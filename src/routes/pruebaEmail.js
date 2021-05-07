7b080ba39cf31154e087b83efca3ae2e629a1bca
Nadia Rosenberg <nadia>
2021-05-06 21:04:19 -0300

    Change completeEmailTemplate to infoEmailTemplate: contains templateEmail context

--- src/routes/pruebaEmail.js
+++ src/routes/pruebaEmail.js
@@ -5,15 +5,9 @@ const sendEmail = require('../utils/emailSender');
 const userData = require('../utils/fakeData');
 const templateEmail = require('../views/layouts/templateEmail')
 const emailsSource = require('../utils/fakeEmailSource');
+const infoEmail = require('../utils/infoEmailTemplate')
 
 const emailSource = emailsSource('newReport');
-
-const templateContext = {
-  title: 'Bienvenida/o a Somos Más',
-  text: 'Hola! Usted se ha registrado correctamente.',
-  contact: '0387 582-7282 / info@fundacionsomosmas.com.ar',
-  url: 'https://scontent.fpra1-1.fna.fbcdn.net/v/t1.18169-9/1452262_1538356629723251_1745439727_n.png?_nc_cat=109&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=b9bX72nxBsYAX9tyuhd&_nc_ht=scontent.fpra1-1.fna&oh=aaea0c2020910da448949253af6aa0b5&oe=60BA5FB3',
-};
 
 //For testing
 router.post('/', async(req, res, next) => {
@@ -23,7 +17,7 @@ router.post('/', async(req, res, next) => {
       to: 'nadiarosenberg96@gmail.com',
       subject: 'Registro existoso',
       template: 'templateEmail',
-      context: templateContext,
+      context: infoEmail
     };
     sendEmail(emailSource, message);
   } catch (err) {
--- src/utils/infoEmailTemplate.js
+++ src/utils/infoEmailTemplate.js
@@ -5,10 +5,6 @@ const templateContext = {
   url: 'https://scontent.fpra1-1.fna.fbcdn.net/v/t1.18169-9/1452262_1538356629723251_1745439727_n.png?_nc_cat=109&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=b9bX72nxBsYAX9tyuhd&_nc_ht=scontent.fpra1-1.fna&oh=aaea0c2020910da448949253af6aa0b5&oe=60BA5FB3',
 };
 
-const mailInfo = {
-  from: emailSource.email,
-  to: userData.email,
-  subject: 'Registro existoso',
-  template: 'templateEmail',
-  context: templateContext,
-};
+module.exports = templateContext;
+
+
