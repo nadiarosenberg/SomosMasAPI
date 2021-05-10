bdc3be6fd83510e7ef8bd9ea84fa9a4fabeb2524
Christian Sosa <chrisosa@everis.com>
2021-05-10 18:00:29 -0300

    fix auth and response

--- src/utils/authjwt.js
+++ src/utils/authjwt.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const key = require("../utils/key");
 const createToken = require('./createToken')
+
 app.set('key', key.key);
 
 const authenticateToken = (req, res, next) => {
