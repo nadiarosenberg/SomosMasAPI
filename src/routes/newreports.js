7e63d98ad86156dbc35b23045ce660d29d4cebe6
Elias Moura <d.eliasmoura2@gmail.com>
2021-05-10 22:56:50 -0300

    update retrieved data in getOne(id) method

--- src/routes/newreports.js
+++ src/routes/newreports.js
@@ -2,7 +2,7 @@ const newreports = require("../controllers/newreports.js");
 const express = require('express');
 const router = express.Router();
 
-router.get("/", newreports.findAll);
+// router.get("/", newreports.findAll);
 
 router.put("/:id", newreports.update);
 
