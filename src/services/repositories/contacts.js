1ca59ee397e4d0096cec68b9d9984fd06a48244c
Elias Moura <d.eliasmoura2@gmail.com>
2021-05-11 12:51:03 -0300

    add persist(contactToPersist) in services/repositories/contact.js

--- src/services/repositories/contacts.js
+++ src/services/repositories/contacts.js
@@ -1,23 +1,9 @@
 const { Contact } = require('../../models');
 const logger = require('../../utils/pinoLogger');
 
-const errorHandler = require("../../utils/errorHandler");
-const sendEmail = require("../../utils/emailSender");
-const emailsSource = require("../../utils/fakeEmailSource");
-const userData = require("../../utils/fakeData");
-const emailSource = emailsSource("newContact");
-
-
 const persist = async (contactToPersist) => {
   try {
     const result = await Contact.create(contactToPersist);
-	 const message = {
-    from: emailSource.email,
-    to: userData.email,
-    subject: "Your new report was created successfully",
-    text: `${userData.firstName} your new report called ${result.name} was created without a problem.`,
-  };
-  
     return result;
   } catch (error) {
     logger.error(error.message);
