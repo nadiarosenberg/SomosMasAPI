46ea1ff271bcb999e8c55a6c45b29202e10f72ac
Elias Moura <d.eliasmoura2@gmail.com>
2021-05-11 12:52:43 -0300

    add POST /contacts for create a new contact in controllers/contact.js

---
+++ src/controllers/contact.js
@@ -0,0 +1,18 @@
+const expressRouter = require('express').Router();
+const handler = require('../handlers/contact');
+const logger = require('../utils/pinoLogger');
+const isAdmin = require('./middlewares/roleId');
+
+expressRouter.post('/', isAdmin, async (req, res, next) => {
+  try {
+    const contactToCreate = req.body;
+
+    const result = await handler.createContact(contactToCreate);
+    res.status(200).json({ id: result.id, message: 'Contact created successfully' });
+  } catch (error) {
+    logger.error(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports = expressRouter;
