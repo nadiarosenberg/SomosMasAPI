8b4456348d1806adc672211a5e43dc9dd0012d4a
Elias Moura <d.eliasmoura2@gmail.com>
2021-05-10 23:17:48 -0300

    delete routes/newreport.js file

--- src/routes/newreports.js
+++
@@ -1,9 +0,0 @@
-const newreports = require("../controllers/newreports.js");
-const express = require('express');
-const router = express.Router();
-
-// router.get("/", newreports.findAll);
-
-router.put("/:id", newreports.update);
-
-module.exports = router;
