ab2988a9fe520d69920fc417ed79a69a77b46c97
otriel <otrielciani@gmail.com>
2021-05-12 06:51:01 -0300

    added association between social media and organization

--- src/models/socialmedia.js
+++ src/models/socialmedia.js
@@ -4,13 +4,10 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class SocialMedia extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      SocialMedia.hasMany(models.Organization, {
+        as: 'organization'
+      });
     }
   };
   SocialMedia.init({
@@ -20,6 +17,8 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'SocialMedia',
+    timestamps: true,
+    paranoid: true
   });
   return SocialMedia;
 };
