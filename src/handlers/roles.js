936dcdf0dbedc44f2c31fc380229c24576aa4919
Christian Sosa <chrisosa@everis.com>
2021-05-12 16:04:42 -0300

    added role handler

---
+++ src/handlers/roles.js
@@ -0,0 +1,50 @@
+const repository = require("../services/repositories/roles");
+
+const createRole = async (role) => {
+    if (!role.name || !role.description) {
+        return res.status(400).json({
+            error: 'Try again'
+        })
+    }
+    const createdRole = await repository.persist(role);
+    return createdRole;
+};
+
+const getAllRoles = async () => {
+    const roles = await repository.getAll();
+    return roles;
+};
+
+const getRoleById = async (id) => {
+    const role = await repository.getOne(id);
+    return role;
+};
+
+const updateRole = async (id, properties) => {
+    if (!properties.name || !properties.description) {
+        return res.status(400).json({
+            error: 'Try again'
+        })
+    }
+    const role = await repository.update(id, properties);
+    return role;
+};
+
+const deleteRole = async (id) => {
+    const role = await repository.destroy(id);
+    return role;
+};
+
+const retoreRole = async (id) => {
+    const role = await repository.restore(id);
+    return role;
+};
+
+module.exports = {
+    createRole,
+    getAllRoles,
+    getRoleById,
+    updateRole,
+    deleteRole,
+    retoreRole
+};
