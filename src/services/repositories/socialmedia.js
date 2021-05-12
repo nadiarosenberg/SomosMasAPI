a6af6b95a0b8c4160fe8b25dfff481eaafd2dad7
otriel <otrielciani@gmail.com>
2021-05-12 07:24:51 -0300

    added post, get, getone, put, delete function on socialmedia repository

--- src/services/repositories/socialmedia.js
+++ src/services/repositories/socialmedia.js
@@ -1,16 +1,37 @@
 const db = require("./../../models");
-const SocialMedia = db.socialMedia;
+const SocialMedia = db.socialmedia;
 
+const getAll = async () => {
+    const result = await SocialMedia.findAll();
+    return result;
+}
 
-const getOne = async (id) => await SocialMedia.findByPk(id, {paranoid: false});
+const getOne = async (id) => {
+    const result = await SocialMedia.findByPk(id, {paranoid: false});
+    return result;
+}
 
-const create = async (socialmedia) => await SocialMedia.create(socialmedia);
+const create = async (socialmedia) => {
+    const result = await SocialMedia.create(socialmedia);
+    return result;
+}
 
-const update = async (socialmedia, id) => await SocialMedia.update(socialmedia, {where: { id: id }});
+const update = async (socialmedia, id) => {
+    const result = await SocialMedia.update(socialmedia, {
+        where: { id: id }
+      });
+    return result;
+}
 
-const deleteOne = async (id) => await SocialMedia.destroy({where: { id: id }});
+const deleteOne = async (id) => {
+    const result = await SocialMedia.destroy({
+        where: { id: id }
+      });
+    return result;
+}
 
 module.exports = {
+    getAll,
     getOne,
     create,
     update,
