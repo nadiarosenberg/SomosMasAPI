110b0632eebacf514ca5da1af28af42e2a715eba
gaston <sleepwalkgaston08@gmail.com>
2021-05-11 23:47:55 -0300

    improve the functions checkImage and checkName

--- src/controllers/middlewares/common.js
+++ src/controllers/middlewares/common.js
@@ -1,64 +1,67 @@
-const Axios = require('axios')
+const Axios = require('axios');
+const logger = require('../../utils/pinoLogger');
 
-const checkImage = async (req, res) => {
-  // image validation
-  if (req.body.image === undefined) {
-    res.status(400).json({
-      ok: false,
-      msj: 'image is required'
-    })
-    return false
-  }
-  // fileExtension validation
+const downloadImage = async (url) => {
+  const downloadedImage = await Axios({
+    method: 'GET',
+    url: url,
+    responseType: 'stream',
+  });
+  return downloadedImage;
+};
+
+const checkImage = async (req) => {
+  // fileType validation
   const url = req.body.image;
   try {
-    const response = await Axios({
-      method: 'GET',
-      url: url,
-      responseType: 'stream'
-    })
-    const fileExtension = await response.headers['content-type'].split('/')[0] || null
-    if (fileExtension === 'image') {
-      return true
-    } else {
-      return false
-    }
-  } catch (e) {
-    res.status(400).json({
-      ok: false,
-      msj: 'invalid image'
-    })
-    return false
+    const downloadedImage = await downloadImage(url);
+    const fileType = downloadedImage.headers['content-type'].split('/')[0] || null;
+    return fileType === 'image' ? true : false;
+  } catch (error) {
+    logger.error(error.message)
+    return false;
   }
-}
+};
 
 const checkName = async (req, res) => {
   // name validation
-  if (req.body.name === undefined) {
+  if (!req.body.name) {
     return res.status(400).json({
       ok: false,
-      msj: 'name is required'
-    })
-  } else if (req.body.name.trim() === "") {
+      msj: 'name is required',
+    });
+  } else if (req.body.name.trim() === '') {
     return res.status(400).json({
       ok: false,
-      msj: 'must be at least one character'
-    })
+      msj: 'must be at least one character',
+    });
   } else {
-    return true
+    return true;
   }
-}
+};
 
 const checkIdInPath = (req, res, next) => {
   if (isNaN(req.params.id)) {
-    res.status(400).send("id must be an integer");
+    res.status(400).send('id must be an integer');
   } else {
     next();
   }
 };
 
+const isValidImage = async (req, res, next) => {
+  const validatedImage = await checkImage(req);
+	if(!validatedImage){
+		res.status(400).json({
+			ok: false, 
+			msj: 'the file is not an image'
+		});
+  }else{
+    next()
+  }
+}
 module.exports = {
   checkIdInPath,
   checkImage,
-  checkName
+  checkName,
+  isValidImage
 };
