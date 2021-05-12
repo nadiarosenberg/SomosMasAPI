47f2c9b708c15fd9fc854cc5fd3df9035738d1c7
otriel <otrielciani@gmail.com>
2021-05-12 08:03:13 -0300

    added social media on getOne controller

--- src/controllers/organizations.js
+++ src/controllers/organizations.js
@@ -7,16 +7,16 @@ const { orgValidationRules, validate } = require('./middlewares/organizations');
 
 expressRouter.post('/', roleIdMiddleware, orgValidationRules(), validate, async (req, res, next) => {
   try {
-      const organizationToCreate = req.body;
-      const result = await handler.createOrganization(organizationToCreate);
-      logger.info({ id: result.id }, 'Organization created successfully')
-      res.status(201).json({
-        id: result.id,
-        message: 'Organization created successfully'
-      })
+    const organizationToCreate = req.body;
+    const result = await handler.createOrganization(organizationToCreate);
+    logger.info({ id: result.id }, 'Organization created successfully')
+    res.status(201).json({
+      id: result.id,
+      message: 'Organization created successfully'
+    })
   } catch (error) {
     logger.error(error.message);
-    res.status(500).json({ message: error.message})
+    res.status(500).json({ message: error.message })
   }
 });
 
@@ -26,7 +26,7 @@ expressRouter.get('/', roleIdMiddleware, async (req, res, next) => {
     res.status(200).json(results);
   } catch (error) {
     logger.error(error.message);
-    res.status(500).json({ message: error.message});
+    res.status(500).json({ message: error.message });
   }
 });
 
@@ -37,13 +37,24 @@ expressRouter.get('/public/:id', roleIdMiddleware, async (req, res, next) => {
     if (!organization) {
       logger.warn('Organization not found');
       res.status(404).json({ message: 'Organization not found' });
-      return;
     }
     const slides = await handlerSlides.getSlidesByOrgId(id);
-    res.status(200).json({"organizationInfo": organization, "slides": slides});
+
+    if (!organization.socialMediaId) {
+      res.status(200).json({ organization, slides });
+      return;
+    }
+    
+    const socialMedia = await handlerSocialMedia.getOneSocialMedia(organization.dataValues.socialMediaId);
+    res.status(200).json({
+      organization,
+      slides,
+      socialMedia
+    });
+
   } catch (error) {
     logger.error(error.message);
-    res.status(500).json({ message: error.message});
+    res.status(500).json({ message: error.message });
   }
 });
 
@@ -65,7 +76,7 @@ expressRouter.put('/public/:id', roleIdMiddleware, async (req, res, next) => {
     res.status(200).json({ message: 'Organization updated successfully' });
   } catch (error) {
     logger.error(error.message);
-    res.status(500).json({ message: error.message});
+    res.status(500).json({ message: error.message });
   }
 });
 
@@ -87,7 +98,7 @@ expressRouter.delete('/:id', roleIdMiddleware, async (req, res, next) => {
     res.status(200).json({ message: 'Organization deleted successfully' });
   } catch (error) {
     logger.error(error.message);
-    res.status(500).json({ message: error.message});
+    res.status(500).json({ message: error.message });
   }
 })
 
@@ -98,7 +109,7 @@ expressRouter.post('/:id', roleIdMiddleware, async (req, res, next) => {
     const organizationDeleted = await handler.retoreOrganization(id);
     console.log(organizationDeleted);
     logger.info('Organization restored successfully')
-    res.status(200).json({ message: 'Organization restored successfully'})
+    res.status(200).json({ message: 'Organization restored successfully' })
   } catch (error) {
     logger.error(error.message);
     res.status(500).json({ message: error.message });
