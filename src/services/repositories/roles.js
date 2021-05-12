e93199795c5663e48a8883c35a7d3104e0fea97a
Christian Sosa <chrisosa@everis.com>
2021-05-12 16:05:10 -0300

    added role repositorie

---
+++ src/services/repositories/roles.js
@@ -0,0 +1,87 @@
+const {
+    Role
+} = require('../../models/index');
+const logger = require('../../utils/pinoLogger');
+
+const persist = async (organizationToPersist) => {
+    try {
+        const result = await Role.create(organizationToPersist);
+        return result;
+    } catch (error) {
+        logger.error(error.message);
+    }
+};
+
+const getAll = async () => {
+    try {
+        const result = await Role.findAll({
+            attributes: ['name', 'description']
+        });
+        return result;
+    } catch (error) {
+        logger.error(error.message);
+    }
+};
+
+const getOne = async (id) => {
+    try {
+        const result = await Role.findOne({
+            where: {
+                id
+            },
+            attributes: ['name', 'description']
+        });
+
+        return result;
+    } catch (error) {
+        logger.error(error.message);
+    }
+};
+
+const update = async (id, properties) => {
+    try {
+        const result = await Role.update(properties, {
+            where: {
+                id
+            }
+        });
+        return result;
+    } catch (error) {
+        logger.error(error.message);
+    }
+};
+
+const destroy = async (id) => {
+    try {
+        const result = await Role.destroy({
+            where: {
+                id
+            }
+        });
+        return result;
+    } catch (error) {
+        logger.error(error.message);
+    }
+};
+
+const restore = async (id) => {
+    try {
+        const result = await Role.restore({
+            where: {
+                id
+            }
+        })
+        return result;
+    } catch (error) {
+        logger.error(error.message);
+    }
+}
+
+module.exports = {
+    persist,
+    update,
+    getAll,
+    getOne,
+    destroy,
+    restore
+};
