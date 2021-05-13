f0250995991d1f47202a57ddf6389fe53137c85c
cdefrancisco <e_defrancisco@yahoo.com.ar>
2021-05-13 00:09:18 -0300

    new architecture

--- src/controllers/contact.js
+++ src/controllers/contact.js
@@ -4,15 +4,26 @@ const logger = require('../utils/pinoLogger');
 const isAdmin = require('./middlewares/roleId');
 const { contactValidationRules, validate } = require('./middlewares/contactValidation');
 
-expressRouter.post('/', isAdmin, contactValidationRules(), validate, async (req, res, next) => {
+expressRouter.post('/',  async (req, res, next) => {
   try {
     const contactToCreate = req.body;
-
+console.log(contactToCreate);
     const result = await handler.createContact(contactToCreate);
     res.status(200).json({ id: result.id, message: 'Contact created successfully' });
   } catch (error) {
     logger.error(error.message);
     res.status(500).json({ message: error.message });
+  }
+});
+
+
+expressRouter.get('/', async (req, res) => {
+  try {
+    const results = await handler.getAllContacts();
+    res.status(200).json(results);
+  } catch (error) {
+    logger.error(error.message);
+    res.status(500).json({ message: error.message || "Some error occurred while retrieving Contacts."});
   }
 });
 
---
+++ src/services/repositories/contact.js
@@ -0,0 +1,39 @@
+const { Contact } = require('../../models');
+const logger = require('../../utils/pinoLogger');
+
+const errorHandler = require("../../utils/errorHandler");
+const sendEmail = require("../../utils/emailSender");
+const emailsSource = require("../../utils/fakeEmailSource");
+const userData = require("../../utils/fakeData");
+const emailSource = emailsSource("newContact");
+
+
+const persist = async (contactToPersist) => {
+  try {
+    const result = await Contact.create(contactToPersist);
+	 const message = {
+    from: emailSource.email,
+    to: userData.email,
+    subject: "Your new report was created successfully",
+    text: `${userData.firstName} your new report called ${result.name} was created without a problem.`,
+  };
+  sendEmail(emailSource, message);
+    return result;
+  } catch (error) {
+    logger.error(error.message);
+  }
+};
+
+const getAll = async () => {
+  try {
+    const result = await Contact.findAll({});
+    return result;
+  } catch (error) {
+    logger.error(error.message);
+  }
+}
+
+module.exports = {
+  persist,
+  getAll
+}
