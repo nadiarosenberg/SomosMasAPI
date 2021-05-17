c259045d768cb6365751df41f90f275258f4ef24
otriel <otrielciani@gmail.com>
2021-05-17 13:59:02 -0300

    deleted findAll funct in socialMedia, added socialMedia service in organization handler , deleted handler socialMedia, used camelcase con index

--- src/services/repositories/socialmedia.js
+++ src/services/repositories/socialmedia.js
@@ -1,7 +1,6 @@
 const db = require("./../../models");
 const SocialMedia = db.socialmedia;
 
-const getAll = async () => await SocialMedia.findAll();
 
 const getOne = async (id) => await SocialMedia.findByPk(id, {paranoid: false});
 
@@ -12,7 +11,6 @@ const update = async (socialmedia, id) => await SocialMedia.update(socialmedia, 
 const deleteOne = async (id) => await SocialMedia.destroy({where: { id: id }});
 
 module.exports = {
-    getAll,
     getOne,
     create,
     update,
