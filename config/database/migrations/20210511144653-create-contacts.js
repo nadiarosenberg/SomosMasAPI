f7acb847b167a5f198e83bfba73fe97af1f5ce6d
cdefrancisco <e_defrancisco@yahoo.com.ar>
2021-05-17 23:03:20 -0300

    fix errors

--- config/database/migrations/20210511144653-create-contacts.js
+++ config/database/migrations/20210511144653-create-contacts.js
@@ -3,7 +3,7 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('Contacts', {
       id: {
-        allowNull: false,
+        
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,15 +1,14 @@
-
 const expressRouter = require('express').Router();
 const handler = require('../handlers/contacts');
 const logger = require('../utils/pinoLogger');
 const isAdmin = require('./middlewares/auth');
 const { contactValidationRules, validate } = require('./middlewares/contactValidation');
 
-expressRouter.post('/', contactValidationRules(), validate, async (req, res, next) => {
+expressRouter.post('/', async (req, res) => {
   try {
     const contactToCreate = req.body;
-
     const result = await handler.createContact(contactToCreate);
+	console.log(result.id);
     res.status(200).json({ id: result.id, message: 'Contact created successfully' });
   } catch (error) {
     logger.error(error.message);
