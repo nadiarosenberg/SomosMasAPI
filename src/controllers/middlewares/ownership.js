deed75d850aafd9b27f1d64d58572e653d534607
cdefrancisco <e_defrancisco@yahoo.com.ar>
2021-05-19 10:19:10 -0300

    moving file

--- src/controllers/middlewares/ownership.js
+++ src/controllers/middlewares/ownership.js
@@ -1,3 +1,19 @@
+/*Buenas, modifiquemos el middleware para que quede simil a este que refactorizamos entre todos: src\controllers\middlewares\auth.js
+
+Los puntos a contemplar son:
+
+    Remover los comentarios
+
+    Utilizar un token que venga en el header
+
+    Utilizar el User ID del token des-serializado
+
+    Llamar a next() en vez de devolver la respuesta al endpoint directamente desde el middleware
+
+    El middleware debe quedar dentro de la carpeta controllers/middlewares (esta carpeta aparecio luego de nuestro refactor pero ya que pedimos los otros cambios podemos hacerlo)
+
+    Rebasear la branch contra DEV
+*/
 const jwt = require('jsonwebtoken');
 const express = require("express");
 const app = express();
@@ -14,14 +30,15 @@ ownerShip.use((req, res, next) => {
       if (err) {
         return res.json({ mensaje: 'invalid Token ' });
       } else {
-        console.log(decoded.sub);
+        console.log(decoded.id);
 		console.log(id);
-        if ((decoded.role === "1") || (decoded.sub===id)) {
+        if ((decoded.role === "1") || (decoded.id===id)) {
 
           req.decoded = decoded;
           return res.json({ mensaje: 'endpoints allowed' });
 
           next();
+		  
         }
         else {
           return res.json({ mensaje: 'error 403' });
