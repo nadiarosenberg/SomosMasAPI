f36606a26d05c3154a324d8887932784e26a5a66
Nadia Rosenberg <nadia>
2021-05-19 22:25:44 -0300

    Modifies pagination

--- src/utils/pagination.js
+++ src/utils/pagination.js
@@ -2,51 +2,52 @@ const getPaginationInfo = (body) => {
   const paginationInfo = {
     page: parseInt(body.page) || 1,
     limit: parseInt(body.pageSize) || 10,
-    order: body.order || 'ASC'
+    order: body.order 
   }
+  if (paginationInfo.order != 'DESC') paginationInfo.order = 'ASC'
   return paginationInfo;
 };
 
 const getOffSet = (paginationInfo) => {
-    const offset = paginationInfo.page * paginationInfo.limit - paginationInfo.limit
-    return offset;
+  const offset = paginationInfo.page * paginationInfo.limit - paginationInfo.limit
+  return offset;
 };
 
-const getNextPage = (paginationInfo, count) => {
+const getNextPage = (paginationInfo, route, count) => {
   let rest = count - paginationInfo.page * paginationInfo.limit
   if(rest >0){
-    let page = paginationInfo.page;
-    return page+1;
+    let nextPage = route+(paginationInfo.page+1);
+    return nextPage;
   }else{
     return null;
   }
 };
 
-const getPreviousPage = (page) => {
-  if (page <= 1) {
+const getPreviousPage = (page, route) => {
+  if(page <= 1){
     return null;
   }
-  return page-1;
+  return route+(page-1);
 };
 
 const getPaginationParams = (paginationInfo) => {
-    const paginationParams = {
-        limit: paginationInfo.limit || null,
-        offset: getOffSet(paginationInfo) || null,
-        order: paginationInfo.order || 'ASC'
-    }
-    return paginationParams;
+  const paginationParams = {
+    limit: paginationInfo.limit || null,
+    offset: getOffSet(paginationInfo) || null,
+    order: paginationInfo.order || 'ASC'
+  }
+  return paginationParams;
 };
 
 const getPaginationResult = (paginationInfo, route, results) => {
-    results.prev = route+getPreviousPage(paginationInfo.page),
-    results.current = paginationInfo.page,
-    results.next = route+getNextPage(paginationInfo, results.count)
-    return results; 
+  results.prev = getPreviousPage(paginationInfo.page, route),
+  results.current = paginationInfo.page,
+  results.next = getNextPage(paginationInfo, route, results.count)
+  return results; 
 };
 
 module.exports = {
-    getPaginationParams,
-    getPaginationResult,
-    getPaginationInfo
+  getPaginationParams,
+  getPaginationResult,
+  getPaginationInfo
 };
