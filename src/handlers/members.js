913f3c7e1ef5ba5d09937dfd50c82516072f8e77
gaston <sleepwalkgaston08@gmail.com>
2021-05-20 17:06:35 -0300

    refactor member pagination to use middleware pagination

--- src/handlers/members.js
+++ src/handlers/members.js
@@ -1,20 +1,7 @@
 const repository = require('../services/repositories/members');
 const { getNextPage, getPriorPage } = require('./middlewares/pagination');
 
-const getAllMembers = async (page, limit) => {
-  if(!page) page = 1;
-  if(!limit) limit = 10;
-  const fromMember = page * limit - limit;
-  const { count, rows } = await repository.getAll(fromMember, limit);
-  const nextPage = getNextPage(count, limit, page, 'members');
-  const priorPage = getPriorPage(page, 'members');
-  return {
-    count,
-    rows,
-    nextPage,
-    priorPage
-  };
-};
+const getAllMembers = async (paginationInfo) => await repository.getAll(paginationInfo);
 
 const createMember = async (member) => {
   const createdMember = await repository.persist(member);
